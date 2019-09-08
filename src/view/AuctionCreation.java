9e314e6819c8bf66cb3ca649bff36cf5992a6be9
Massimiliano Marchiano <m.marchiano2@campus.unimib.it>
2019-09-08 18:21:21 +0200

    First commit

---
+++ src/view/AuctionCreation.java
@@ -0,0 +1,53 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuctionCreation
+ */
+@WebServlet("/AuctionCreation")
+public class AuctionCreation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuctionCreation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"Menu.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<div class=\"topnav\">");
+		out.println("<div class=\"menu-trigger\"></div>");
+		out.println("</div>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
