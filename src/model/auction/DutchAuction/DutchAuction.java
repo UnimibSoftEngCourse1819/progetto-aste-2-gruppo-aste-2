fa3faa0018bb967048f4d14503923302a5dc2751
Massimiliano Marchiano <m.marchiano2@campus.unimib.it>
2019-09-10 21:03:07 +0200

    Modificata classe DutchAuction

--- src/model/auction/DutchAuction/DutchAuction.java
+++ src/model/auction/DutchAuction/DutchAuction.java
@@ -8,18 +8,29 @@ import model.Operation;
 import model.auction.Auction;
 
 public class DutchAuction extends Auction {
+	private long minPrice;
 	private long currentPrice;
+	private long amount;
+	private DutchOffer offer;
+	
+	public void adjudicate(User bidder) {
+		offer = new DutchOffer(bidder, currentPrice);
+	}
+	
+	public void decreasePrice() {
+		if(currentPrice > minPrice && (currentPrice - amount) >= currentPrice)
+			currentPrice = currentPrice - amount;
+	}
 	
 	@Override
-	public void addOffer(Offer newOffer) throws IncompatibilityClassException {
-		// TODO Auto-generated method stub
-		
-	}
+	public void addOffer(Offer newOffer) throws IncompatibilityClassException {	}
 
 	@Override
 	public List<Operation> end() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Operation> operationToDo = new ArrayList<>();
+		operationToDo.add(new Transaction(offer.getBidder(), seller, currentPrice()));
+		
+		return operationToDo;
 	}
 
 }
