4235fc08dcfa2261d61be0890a974a96d5d5189d
Massimiliano Marchiano <m.marchiano2@campus.unimib.it>
2019-09-18 11:47:28 +0200

    Sistemato path del database

--- src/controller/database/Connector.java
+++ src/controller/database/Connector.java
@@ -10,17 +10,16 @@ import java.sql.SQLException;
 import exception.MyConnectionException;
 
 public class Connector {
-	private static final String path = "database" + File.separator;
+	private static final String path = File.separator + "database" + File.separator;
 	private static final String DATABASE_DRIVER = "jdbc:sqlite:";
-//	private static final String 
 	private static final String DATABASE_NAME = "asta2.db";
 	
 	public static Connection getConnection() throws MyConnectionException, IOException {
 		try {
 			Class.forName("org.sqlite.JDBC");
-			String sqlScriptUrl = Connector.class.getClassLoader().getResource("").getPath();
-			System.out.println(sqlScriptUrl);
-			return DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Massi\\eclipse-workspace\\ProgettoAsteOnline\\database\\asta2.db");
+			String sqlScriptUrl = System.getProperty("user.dir");
+
+			return DriverManager.getConnection(Connector.DATABASE_DRIVER + sqlScriptUrl + Connector.path + Connector.DATABASE_NAME);
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new MyConnectionException();
 		}
